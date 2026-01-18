f6b5a4219db8b9700a8356041ffcd70c483e2754
Zaida Wiss <zaida.wiss@chasacademy.se>
2026-01-18 20:42:09 +0100

    uppdaterade Dashboard med Controlled input ✅
    Form + onSubmit ✅
    
    preventDefault korrekt använd ✅
    
    Trim + stoppa tomma värden ✅
    
    Functional state update (prevGuesses) ✅
    
    Rendera lista med map + key ✅

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 const Dashboard = ({ title }) => {
   const [count, setCount] = useState(0);
   const [guess, setGuess] = useState("hej");
+  const [guesses, setGuesses] = useState([]);
 
   useEffect(() => {
   if (count > 0) {
@@ -11,26 +12,42 @@ const Dashboard = ({ title }) => {
   }
 }, [count]);
 
-  useEffect(() => {
+useEffect(() => {
   if (guess.trim().length > 0) {
     // logik här
-console.log("Du gissade:", guess);
+    console.log("Du gissade:", guess);
   }
 }, [guess]);
 
+const handleNewGuess = (event) => {
+  event.preventDefault();           // 1. stoppa reload
 
-  return (
-    <div>
+  const cleanedGuess = guess.trim();
+  if (!cleanedGuess) return;
+
+  setGuesses((prevGuesses) => [...prevGuesses, cleanedGuess]);  // 2. lägg till gissning
+  setGuess("");                     // 3. töm input
+};
+
+return (
+  <div>
       <h1>{title}</h1>
       <p>Count: {count}</p>
       <button onClick={() => setCount(count + 1)}>
         Öka
       </button>
-<input
-  type="text"
-  value={guess}
-  onChange={(event) => setGuess(event.target.value)}
-/>
+      <form onSubmit={handleNewGuess}>
+        <input
+          type="text"
+          value={guess}
+          onChange={(event) => setGuess(event.target.value)}  />
+        <button type="submit">Gissa</button>
+      </form>
+      <ul>
+        {guesses.map((g, index) => (
+          <li key={index}>{g}</li>
+        ))}
+      </ul>
 
     </div>
   );
